f9c8227558fc9ddb776351ad4dad5318f36c16e0
EspenSnerten <espensnert@hotmail.com>
2023-08-29 14:25:46 +0200

    test to see if it works on the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,6 +4,11 @@ function ProfilePage() {
   return (
     <>
       <ExampleProfile />
+
+      <div className="bg-blue-200 p-4">
+      <h1 className="text-2xl font-bold text-blue-600">Hello Tailwind CSS!</h1>
+      <p className="mt-2 text-gray-700">Welcome to your Tailwind CSS powered React app.</p>
+    </div>
     </>
   );
 }
