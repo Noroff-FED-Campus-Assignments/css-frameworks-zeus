2ba35fa79e692ee80a7d1b7bb1ea7f5c079d5b0c
sanderselfors <sanost93689@stud.noroff.no>
2023-08-30 11:16:21 +0200

    Added form

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import { Outlet } from "@tanstack/react-router";
+import { Outlet, Link } from "@tanstack/react-router";
 import "./App.scss";
 
 function App() {
   return (
     <>
-
+<header>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/Login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
       <main>
         <Outlet />
       </main>
