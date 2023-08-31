7d1a0284c62d1e9273f5951fddbaf1a7fc2a5baf
EspenSnerten <espensnert@hotmail.com>
2023-08-31 20:20:52 +0200

    gen style update
    - added poppins font
    - gen style on the profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,18 +4,20 @@ import hannahwebster from "../assets/hannahwebster.jpg";
 import heidiparker from "../assets/heidiparker.jpg";
 import millieburton from "../assets/millieburton.jpg";
 import johnharris from "../assets/johnharris.jpg";
+import React, { useState } from "react";
+
 
 function ProfilePage() {
   return (
     <>
       <main className="bg-white p-4 flex flex-col min-h-screen items-center justify-center mx-auto max-w-7xl">
         <div className="flex flex-row w-full justify-start">
-        <h3 className="text-2xl font-bold text-black mb-4 tracking-wide">
-          Bobby Webb
-        </h3>
+          <h3 className="text-2xl font-semibold text-black mb-4 tracking-wide">
+            Bobby Webb
+          </h3>
         </div>
         <div className="md:flex md:items-center justify-center gap-4 w-full">
-          <div className="w-full md:w-4/5">
+          <div className="w-full md:w-4/5 relative">
             <img
               src={bobbywebb}
               alt="A picture of the man named Bobby Webb"
@@ -44,43 +46,43 @@ function ProfilePage() {
             <h4 className="text-lg font-semibold">Contacts</h4>
             <div className="">
               <ul className="flex flex-row md:flex md:flex-col gap-8 md:gap-8 overflow-x-auto flex-nowrap ">
-                <div className="flex flex-col md:flex md:flex-row items-center gap-2">
+                <div className="flex flex-col md:flex md:flex-row items-center gap-2 cursor-pointer hover:font-semibold duration-100 transform">
                   <img
                     src={hannahwebster}
-                    alt="A picture of the woman Hannah Webster"
-                    className="w-16 h-16 rounded-full object-cover"
+                    alt="A picture of the woman named Hannah Webster"
+                    className="w-16 h-16 rounded-3xl object-cover"
                   />
                   <p className="truncate">Hannah Webster</p>
                 </div>
-                <div className="flex flex-col md:flex md:flex-row items-center gap-2">
+                <div className="flex flex-col md:flex md:flex-row items-center gap-2 cursor-pointer hover:font-semibold duration-100 transform">
                   <img
                     src={heidiparker}
-                    alt="A picture of the woman Heidi Parker"
-                    className="w-16 h-16 rounded-full object-cover"
+                    alt="A picture of the woman named Heidi Parker"
+                    className="w-16 h-16 rounded-3xl object-cover"
                   />
                   <p className="truncate">Heidi Parker</p>
                 </div>
-                <div className="flex flex-col md:flex md:flex-row items-center gap-2">
+                <div className="flex flex-col md:flex md:flex-row items-center gap-2 cursor-pointer hover:font-semibold duration-100 transform">
                   <img
                     src={millieburton}
-                    alt="A picture of the woman Millie Burton"
-                    className="w-16 h-16 rounded-full object-cover"
+                    alt="A picture of the woman named Millie Burton"
+                    className="w-16 h-16 rounded-3xl object-cover"
                   />
                   <p className="truncate">Millie Burton</p>
                 </div>
-                <div className="flex flex-col md:flex md:flex-row items-center gap-2">
+                <div className="flex flex-col md:flex md:flex-row items-center gap-2 cursor-pointer hover:font-semibold duration-100 transform">
                   <img
                     src={johnharris}
-                    alt="A picture of the man John Harris"
-                    className="w-16 h-16 rounded-full object-cover"
+                    alt="A picture of the man named John Harris"
+                    className="w-16 h-16 rounded-3xl object-cover"
                   />
                   <p className="truncate">John Harris</p>
                 </div>
-                <div className="flex flex-col md:flex md:flex-row items-center gap-2">
+                <div className="flex flex-col md:flex md:flex-row items-center gap-2 cursor-pointer hover:font-semibold duration-100 transform">
                   <img
                     src={davidharker}
-                    alt="A picture of the man David Harker"
-                    className="w-16 h-16 rounded-full object-cover"
+                    alt="A picture of the man named David Harker"
+                    className="w-16 h-16 rounded-3xl object-cover"
                   />
                   <p className="truncate">David Harker</p>
                 </div>
@@ -90,13 +92,15 @@ function ProfilePage() {
 
           <div className="flex flex-col">
             <div className="space-y-4 bg-slate-50 md:p-4 md:pr-20 md:pl-10 md:py-10 rounded flex flex-col  gap-4">
-              <h4 className="text-lg font-semibold px-6 md:px-0 pt-4 md:pt-0">New posts</h4>
+              <h4 className="text-lg font-semibold px-6 md:px-0 pt-4 md:pt-0">
+                New posts
+              </h4>
               <div className="space-y-2 border-2 border-t-blue-500 border-x-transparent border-b-transparent">
-                <div className="flex items-center space-x-2 mt-4 px-4 md:px-0">
+                <div className="flex items-center space-x-2 mt-4 px-4 md:px-0 cursor-pointer">
                   <img
                     src={hannahwebster}
-                    alt=""
-                    className="w-20 h-20 rounded-full object-cover"
+                    alt="A picture of the woman named Hannah Webster"
+                    className="w-20 h-20 rounded-3xl object-cover"
                   />
                   <h5 className="font-semibold">Hannah Webster</h5>
                 </div>
@@ -107,11 +111,11 @@ function ProfilePage() {
                 </p>
               </div>
               <div className="space-y-2 border-2 border-t-blue-500 border-x-transparent border-b-transparent">
-                <div className="flex items-center space-x-2 mt-4 px-4 md:px-0">
+                <div className="flex items-center space-x-2 mt-4 px-4 md:px-0 cursor-pointer">
                   <img
                     src={bobbywebb}
-                    alt=""
-                    className="w-20 h-20 rounded-full object-cover"
+                    alt="A picture of the man named Bobby Webb"
+                    className="w-20 h-20 rounded-3xl object-cover"
                   />
                   <h5 className="font-semibold">Bobby Webb</h5>
                 </div>
@@ -131,10 +135,10 @@ function ProfilePage() {
                 <textarea
                   id="newpost"
                   rows="1"
-                  className="w-11/12 md:mx-2 my-1 p-2 bg-gray-300 placeholder-black rounded align-middle	resize-none"
+                  className="w-11/12 md:mx-2 my-1 p-2 bg-gray-200 placeholder-black rounded align-middle	resize-none"
                   placeholder="What do you want to share..."
                 ></textarea>
-                <button className="w-11/12 md:w-2/5 my-1  md:mx-2 p-2 bg-blue-600 hover:bg-blue-700 text-white rounded font-semibold">
+                <button className="w-11/12 md:w-2/5 my-1  md:mx-2 p-2 bg-blue-600 hover:bg-blue-700 duration-200 transform text-white rounded font-semibold">
                   Submit
                 </button>
               </form>
